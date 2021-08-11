115c4a62f28c1318efb271d3651241a64e9c7c1c
Viktor Martynov <2013alfabravo@gmail.com>
2021-08-12 03:13:37 +0400

    character list sample

--- app/src/main/java/com/github/alfabravo2013/marvelcharacters/presentation/home/CharacterListAdapter.kt
+++ app/src/main/java/com/github/alfabravo2013/marvelcharacters/presentation/home/CharacterListAdapter.kt
@@ -8,7 +8,38 @@ import com.github.alfabravo2013.marvelcharacters.databinding.CharacterListItemBi
 import com.github.alfabravo2013.marvelcharacters.presentation.models.CharacterListItem
 
 class CharacterListAdapter : RecyclerView.Adapter<CharacterListAdapter.ViewHolder>() {
-    private val characterList: MutableList<CharacterListItem> = mutableListOf()
+    private val characterList: MutableList<CharacterListItem> = mutableListOf(
+        CharacterListItem(
+            id = 1,
+            name = "3-D Man",
+            imageUrl = "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+        ),
+        CharacterListItem(
+            id = 2,
+            name = "A-Bomb (HAS)",
+            imageUrl = "http://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16.jpg"
+        ),
+        CharacterListItem(
+            id = 1,
+            name = "3-D Man",
+            imageUrl = "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+        ),
+        CharacterListItem(
+            id = 2,
+            name = "A-Bomb (HAS)",
+            imageUrl = "http://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16.jpg"
+        ),
+        CharacterListItem(
+            id = 1,
+            name = "3-D Man",
+            imageUrl = "http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+        ),
+        CharacterListItem(
+            id = 2,
+            name = "A-Bomb (HAS)",
+            imageUrl = "http://i.annihil.us/u/prod/marvel/i/mg/3/20/5232158de5b16.jpg"
+        )
+    )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = CharacterListItemBinding
