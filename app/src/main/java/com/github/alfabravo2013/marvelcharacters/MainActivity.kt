915aa9d61c6d0c536e405cbe37f737fea165bf6f
Viktor Martynov <2013alfabravo@gmail.com>
2021-08-11 21:03:24 +0400

    set up navigation

--- app/src/main/java/com/github/alfabravo2013/marvelcharacters/MainActivity.kt
+++ app/src/main/java/com/github/alfabravo2013/marvelcharacters/MainActivity.kt
@@ -2,10 +2,28 @@ package com.github.alfabravo2013.marvelcharacters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.github.alfabravo2013.marvelcharacters.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupWithNavController(binding.activityMainToolbar, navController, appBarConfiguration)
     }
 }
